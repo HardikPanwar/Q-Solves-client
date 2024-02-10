a70bbe842bb02a74530d64cdaadf58310b06becc
unknown <gp206524@gmail.com>
2024-02-10 12:05:48 +0530

    kit-ui

---
+++ src/pages/Details/KitDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+
+const KitDetails = () => {
+  return (
+    <div className='home-container-1'>
+        <LeftSidebar />
+        <div className='home-container-2' style={{ marginTop: "30px" }}>KitDetails</div>
+    </div>
+  )
+}
+
+export default KitDetails
